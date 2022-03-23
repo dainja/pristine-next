c0145fe21ce55179c152fc653adca3a3b1b080d5
Max Malm <benjick@dumfan.net>
2022-03-23 19:51:26 +0100

    Lighthouse script

---
+++ tools/lighthouse.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const util = require("util");
+const child_process = require("child_process");
+const exec = util.promisify(child_process.exec);
+
+const port = 3031;
+const page = `http://localhost:${port}/`;
+
+async function runLighthouse() {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const options = {
+    logLevel: "info",
+    output: "html",
+    port: chrome.port,
+  };
+  const runnerResult = await lighthouse(page, options);
+
+  const reportHtml = runnerResult.report;
+  fs.writeFileSync("lhreport.html", reportHtml);
+
+  console.log("✅ Report is done for", runnerResult.lhr.finalUrl);
+  Object.keys(runnerResult.lhr.categories).forEach((key) => {
+    console.log(`${key} score:`, runnerResult.lhr.categories[key].score * 100);
+  });
+  console.log("🧑‍🔬 See lhreport.html for full report");
+
+  await chrome.kill();
+}
+
+async function main() {
+  console.log("🔨 building project...");
+  await exec("yarn build");
+  console.log("✅ build complete!");
+  console.log("🔦 running lighthouse...");
+  const server = child_process.exec(`yarn start --port ${port}`, async () => {
+    await runLighthouse();
+    const killed = server.kill("SIGINT");
+    console.log("killed", killed);
+  });
+  // await Promise.all([exec(`yarn start --port ${port}`), runLighthouse()]);
+}
+
+main();
