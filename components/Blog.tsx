26fdc62bc03afcca25ed266748179cde3fbfc1fe
Max Malm <benjick@dumfan.net>
2022-03-28 22:39:24 +0200

    Extract Blog component

---
+++ components/Blog.tsx
@@ -0,0 +1,132 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: "Cheryl G. Yates",
+    href: "#",
+    date: "Mar 10, 2020",
+    datetime: "2020-03-10",
+    category: { name: "FRISÖR", href: "#" },
+    imageUrl:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80",
+    preview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
+    author: {
+      name: "Brenna Goyette",
+      imageUrl:
+        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      href: "#",
+    },
+    readingLength: "4 min",
+  },
+
+  {
+    id: 2,
+    title: "Adnan D.",
+    href: "#",
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    category: { name: "BARBERARE", href: "#" },
+    imageUrl:
+      "https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80",
+    preview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
+    author: {
+      name: "Daniela Metz",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      href: "#",
+    },
+    readingLength: "11 min",
+  },
+  {
+    id: 3,
+    title: "Theresa P. Meza",
+    href: "#",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { name: "BEAUTY", href: "#" },
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+    preview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
+    author: {
+      name: "Roel Aufderehar",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      href: "#",
+    },
+    readingLength: "6 min",
+  },
+];
+
+export const Blog: React.FC = () => {
+  return (
+    <div className="relative bg-custom1 py-16 sm:py-24 lg:py-32">
+      <div className="relative">
+        <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+          <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase"></h2>
+          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl  font-aurora">
+            Om oss
+          </p>
+          <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500"></p>
+        </div>
+        <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
+          {blogPosts.map((post) => (
+            <div
+              key={post.id}
+              className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+            >
+              <div className="flex-shrink-0">
+                <img
+                  className="h-96 w-full object-cover"
+                  src={post.imageUrl}
+                  alt=""
+                />
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm font-medium text-cyan-600">
+                    <a href={post.category.href} className="hover:underline">
+                      {post.category.name}
+                    </a>
+                  </p>
+                  <a href={post.href} className="block mt-2">
+                    <p className="text-xl font-semibold text-gray-900">
+                      {post.title}
+                    </p>
+                    <p className="mt-3 text-base text-gray-500">
+                      {post.preview}
+                    </p>
+                  </a>
+                </div>
+                {/* <div className="mt-6 flex items-center">
+                    <div className="flex-shrink-0">
+                      <a href={post.author.href}>
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={post.author.imageUrl}
+                          alt={post.author.name}
+                        />
+                      </a>
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                        <a href={post.author.href} className="hover:underline">
+                          {post.author.name}
+                        </a>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                        <time dateTime={post.datetime}>{post.date}</time>
+                        <span aria-hidden="true">&middot;</span>
+                        <span>{post.readingLength} read</span>
+                      </div>
+                    </div>
+                  </div> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
