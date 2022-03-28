c9aaa7c82fe73113c938920323abb1ab622f4d0e
Daniel Najafi <dainja@protonmail.com>
2022-03-28 17:54:49 +0200

    add testimonials

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,45 +20,8 @@ import { ChevronRightIcon, ExternalLinkIcon } from "@heroicons/react/solid";
 import plogo from "../assets/plogo-svg.svg";
 import Image from "next/image";
 import Head from "next/head";
+import { Testimonials } from "../components/Testimonials";
 
-const features = [
-  {
-    name: "Push to Deploy",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.",
-    icon: CloudUploadIcon,
-  },
-  {
-    name: "SSL Certificates",
-    description:
-      "Qui aut temporibus nesciunt vitae dicta repellat sit dolores pariatur. Temporibus qui illum aut.",
-    icon: LockClosedIcon,
-  },
-  {
-    name: "Simple Queues",
-    description:
-      "Rerum quas incidunt deleniti quaerat suscipit mollitia. Amet repellendus ut odit dolores qui.",
-    icon: RefreshIcon,
-  },
-  {
-    name: "Advanced Security",
-    description:
-      "Ullam laboriosam est voluptatem maxime ut mollitia commodi. Et dignissimos suscipit perspiciatis.",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Powerful API",
-    description:
-      "Ab a facere voluptatem in quia corrupti veritatis aliquam. Veritatis labore quaerat ipsum quaerat id.",
-    icon: CogIcon,
-  },
-  {
-    name: "Database Backups",
-    description:
-      "Quia qui et est officia cupiditate qui consectetur. Ratione similique et impedit ea ipsum et.",
-    icon: ServerIcon,
-  },
-];
 const blogPosts = [
   {
     id: 1,
@@ -298,51 +261,7 @@ const Home: NextPage = () => {
       </div>
 
       {/* Testimonial section */}
-      <div className="pb-16 bg-custom2 lg:pb-0 lg:z-10 lg:relative">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
-          <div className="relative lg:-my-8">
-            <div
-              aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
-            />
-            <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-              <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-                <img
-                  className="object-cover lg:h-full lg:w-full"
-                  src="https://images.unsplash.com/photo-1622286342621-4bd786c2447c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-          <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
-            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
-              <blockquote>
-                <div>
-                  <svg
-                    className="h-12 w-12 text-white opacity-25"
-                    fill="currentColor"
-                    viewBox="0 0 32 32"
-                    aria-hidden="true"
-                  >
-                    <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                  </svg>
-                  <p className="mt-6 text-2xl font-medium text-white">
-                    Lyxig behandling med varma handdukar, rakning med kniv och
-                    superproffsig frisör/barberare! Rekommenderar Adnan varmt då
-                    han tar sig tid och ger en helhetsupplevelse där jag kände
-                    mig otroligt bortskämd!
-                  </p>
-                </div>
-                <footer className="mt-6">
-                  <p className="text-base font-medium text-white">Anton Y.</p>
-                  <p className="text-base font-medium text-cyan-100"></p>
-                </footer>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Testimonials />
 
       {/* Blog section */}
       <div className="relative bg-custom1 py-16 sm:py-24 lg:py-32">
