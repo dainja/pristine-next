d3024ccdc1aa63603bb498230ecb24d42b50829c
Daniel Najafi <dainja@protonmail.com>
2022-04-01 17:48:07 +0200

    add analytics google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,26 @@ import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { Layout } from "../components/Layout";
 import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../src/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { pathname } = useRouter();
-  if (pathname === "/og-image") {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  if (router.pathname === "/og-image") {
     return <Component {...pageProps} />;
   }
+
   return (
     <Layout>
       <DefaultSeo
@@ -18,6 +32,24 @@ function MyApp({ Component, pageProps }: AppProps) {
           type: "website",
           locale: "sv_SE",
           site_name: "Pristine",
+        }}
+      />
+      <Script
+        strategy="worker"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+            });
+        `,
         }}
       />
       <Component {...pageProps} />
