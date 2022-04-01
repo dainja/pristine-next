d15b3a6e84341abba65cdf784f7a93baaec2d6a7
Max Malm <benjick@dumfan.net>
2022-04-01 16:55:07 +0200

    Fix sitemaps urls

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,7 @@
-const siteUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const siteUrl =
+  process.env.NODE_ENV === "production"
+    ? `https://www.pristinevxo.se`
+    : "http://localhost:3000";
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
