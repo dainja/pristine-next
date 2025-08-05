0601852fd5bbcc5295c89d00bad0417559b13e90
mrdogar009 <mr.dogar009@gmail.com>
2025-08-05 16:29:17 +0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Partytown Config (keep this) */}
         <script
           data-partytown-config
           dangerouslySetInnerHTML={{
@@ -15,8 +16,29 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* GTM - Head Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5RPPV55M');`,
+          }}
+        />
       </Head>
       <body>
+        {/* GTM - NoScript Fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5RPPV55M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
