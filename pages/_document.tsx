b6c22138751d150d4243702aeebb972f981fec4f
alvinrabock <alvin@frontspace.se>
2025-08-18 13:52:21 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,42 +4,29 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/*
-          This is the Partytown configuration. Keep it in the <Head>
-          as it must load before all other scripts.
-        */}
+        {/* Google Tag Manager */}
         <script
-          data-partytown-config
           dangerouslySetInnerHTML={{
             __html: `
-              partytown = {
-                lib: "/_next/static/~partytown/",
-                forward: ["gtag"]
-              };
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5RPPV55M');
             `,
           }}
         />
-
-        <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5RPPV55M');</script>
-<!-- End Google Tag Manager -->
-
       </Head>
       <body>
-                {/* GTM - NoScript Fallback (keep this in the <body>) */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5RPPV55M"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          />
         </noscript>
-
         <Main />
         <NextScript />
       </body>
