4e9df97ade2d9b55db709b745a0d09951096cca0
alvinrabock <alvin@frontspace.se>
2025-08-18 13:50:52 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,9 +19,18 @@ export default function Document() {
             `,
           }}
         />
+
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5RPPV55M');</script>
+<!-- End Google Tag Manager -->
+
       </Head>
       <body>
-        {/* GTM - NoScript Fallback (keep this in the <body>) */}
+                {/* GTM - NoScript Fallback (keep this in the <body>) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5RPPV55M"
